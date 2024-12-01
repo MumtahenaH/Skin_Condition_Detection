6cd201a8cc9a3931eab362a2b73d2c3025736b18
MumtahenaH <manty905@gmail.com>
2024-12-01 08:13:16 +0600

    image upload button fixed

--- js/scripts.js
+++ js/scripts.js
@@ -65,26 +65,4 @@ window.addEventListener('DOMContentLoaded', event => {
         closeCaptureModal();
       }
 
-      // Access the DOM elements
-const video = document.getElementById('camera');
-const canvas = document.getElementById('photo');
-const captureBtn = document.getElementById('capture-btn');
-const context = canvas.getContext('2d');
-
-// Request access to the user's camera
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-        video.srcObject = stream;
-    })
-    .catch((error) => {
-        console.error("Error accessing the camera: ", error);
-    });
-
-// Capture photo on button click
-captureBtn.addEventListener('click', () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-});
-
 });
